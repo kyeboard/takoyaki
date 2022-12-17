f455fc929d2909450bb37dcf5f19a155b8f3f6f0
kyeboard <me@kyeboard.me>
2022-12-17 10:33:29 +0530

    feat: Add NavBar

---
+++ takoyaki_website/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        class="navbar absolute z-10 flex w-full px-20 py-8 font-medium bg-white/40 backdrop-blur-lg animate-fade_in"
+    >
+        <div class="left">
+            <div class="title font-bold text-3xl tracking-wide dancing-script">
+                takoyaki
+            </div>
+        </div>
+        <div class="mx-auto font-semibold text-gray-700 flex gap-x-14">
+            <a href="/">Home</a>
+            <a href="/installation">Installation</a>
+            <a href="/blogs">Documentation</a>
+            <a href="/market">Marketplace</a>
+            <a href="https://www.kyeboard.me/contact">Contact</a>
+        </div>
+    </div>
+</template>
--- takoyaki_website/src/views/HomeView.vue
+++ takoyaki_website/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="bg-white w-screen h-screen"></div>
 </template>
