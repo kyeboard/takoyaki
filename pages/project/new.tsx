5625048e153913f3e4ef96a3b499fa4b1fbae607
kyeboard <me@kyeboard.me>
2023-02-10 11:16:58 +0530

    Allow users to add members to team as a UI update

--- pages/project/new.tsx
+++ pages/project/new.tsx
@@ -2,19 +2,35 @@ import {
     Box,
     Button,
     Flex,
+    Image,
     Input,
     Text,
     Textarea,
 } from "@pankod/refine-chakra-ui";
 import { Nunito } from "@next/font/google";
 import ColorSelection from "@components/ColorInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Mail, Plus } from "react-feather";
+import { storage } from "src/utility";
 
 const nunito = Nunito({ subsets: ["latin"], weight: "800" });
 
 const NewProject = () => {
     const [color, set_color] = useState<string>("pink");
     const [name, set_name] = useState<string>("");
+    const [email, set_email] = useState<string>("");
+    const [users, set_users] = useState<Array<string>>([
+        "me@kyeboard.me",
+        "androiduser@gmail.com",
+        "androiduser@gmail.com",
+        "androiduser@gmail.com",
+    ]);
+
+    const add_user = () => {
+        users.push(email);
+
+        set_email("");
+    };
 
     return (
         <Flex
@@ -48,7 +64,7 @@ const NewProject = () => {
                             height={44}
                             resize="none"
                             marginTop={2}
-                            placeholder="Project Name"
+                            placeholder="A legendary description about your project..."
                         />
                     </Box>
                     <Box marginTop={5}>
@@ -81,22 +97,49 @@ const NewProject = () => {
                     borderWidth={1}
                     borderColor="#DCE0F3"
                 ></Box>
-                <Flex width={"full"} direction="column">
+                <Flex width={"full"} direction="column" position={"relative"}>
                     <Text>Invite members</Text>
-                    <Input
-                        bg="#dde1f3"
-                        padding={6}
-                        marginTop={2}
-                        placeholder="Enter member's email address..."
-                    />
-                    <Flex
-                        height={"full"}
-                        width="full"
-                        alignItems={"center"}
-                        justifyContent="center"
-                    >
-                        <Text>No members yet!</Text>
+                    <Flex marginTop={2} gap={5}>
+                        <Input
+                            bg="#dde1f3"
+                            padding={6}
+                            borderTopRadius={"5"}
+                            borderBottomRadius={"0"}
+                            borderBottom={"solid"}
+                            borderBottomColor={"gray.400"}
+                            value={email}
+                            borderBottomWidth={"2px"}
+                            onChange={(e) => set_email(e.target.value)}
+                            placeholder="Enter member's email address..."
+                        />
+                        <Button
+                            padding={6}
+                            _hover={{ bg: "#2E3440" }}
+                            bg="#2E3440"
+                            color="#D8DEE9"
+                            onClick={add_user}
+                        >
+                            <Plus />
+                        </Button>
                     </Flex>
+                    <Box height={"full"} width="full">
+                        {users.map((email) => {
+                            return (
+                                <Flex
+                                    key={email}
+                                    bg="#dde1f3"
+                                    marginTop={5}
+                                    padding={4}
+                                    borderRadius={10}
+                                    gap={4}
+                                    paddingX={6}
+                                >
+                                    <Mail />
+                                    {email}
+                                </Flex>
+                            );
+                        })}
+                    </Box>
                 </Flex>
             </Flex>
         </Flex>
