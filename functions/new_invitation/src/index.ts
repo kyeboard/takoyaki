25f86aa514036397b39031f72af553da056ef3b8
kyeboard <me@kyeboard.me>
2023-02-17 10:30:29 +0530

    function(new_invitation): Publish new invitation in the user's tab

---
+++ functions/new_invitation/src/index.ts
@@ -0,0 +1,56 @@
+import { Client, Databases, Models } from "node-appwrite";
+
+interface Request {
+    headers: {
+        [key: string]: string;
+    };
+    payload: {
+        [key: string]: string;
+    };
+    variables: {
+        [key: string]: string;
+    };
+}
+
+interface Response {
+    send: (text: string, status?: number) => void;
+    json: (obj: any, status?: number) => void;
+}
+
+const new_invitation = async function (req: Request, res: Response) {
+    const client = new Client();
+
+    // You can remove services you don't use
+    const databases = new Databases(client);
+
+    if (
+        !req.variables["APPWRITE_FUNCTION_ENDPOINT"] ||
+        !req.variables["APPWRITE_FUNCTION_API_KEY"]
+    ) {
+        console.warn(
+            "Environment variables are not set. Function cannot use Appwrite SDK."
+        );
+    } else {
+        client
+            .setEndpoint(req.variables["APPWRITE_FUNCTION_ENDPOINT"])
+            .setProject(req.variables["APPWRITE_FUNCTION_PROJECT_ID"])
+            .setKey(req.variables["APPWRITE_FUNCTION_API_KEY"])
+            .setSelfSigned(true);
+    }
+
+    const data: Models.Membership = JSON.parse(
+        req.variables["APPWRITE_FUNCTION_EVENT_DATA"]
+    );
+
+    await databases.createDocument(data.userName, "invitations", "unique()", {
+        name: data.teamName,
+        accept_url: `https://planetary.kyeboard.me/accept_invitation?teamId=${data.teamId}`,
+    });
+
+    res.json({
+        success: true,
+        message: "New invitation sent",
+    });
+};
+
+export default new_invitation;
